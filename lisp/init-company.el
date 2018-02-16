d7a99493b68fc87cb0f3d1b4f73ccc93dfb7218f
curtuner <curtuner@gmail.com>
2018-02-17 00:57:24 +0800

    update company-mode setting

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,10 +1,14 @@
 (add-hook 'after-init-hook 'global-company-mode)
 
+(setq company-tooltip-limit 10)
+(setq company-minimum-prefix-length 2)
 (setq company-idle-delay 0)
 
 
 
 (global-set-key (kbd "C-c d") 'bing-dict-brief)
 (global-set-key (kbd "C-c o") 'company-complete-common)
+
+
 
 (provide 'init-company)
